993a9f5975a6559b8257f085fb9a28a83fb0e476
GyroW <GyroW@users.noreply.github.com>
2017-02-23 20:04:48 +0100

    Update klok.lua
    Now checking if a/b1 is bigger than a/b2, this changes what we do with a/bomlaag

--- klok.lua
+++ klok.lua
@@ -14,16 +14,26 @@ for i=1, n do
 	b2 = nums[2]
 	d = 0
 	aomhoog = math.abs(a1-a2)
-	aomlaag = math.abs(a1-(24+a2))
+	if a1 < a2 then
+		aomlaag = math.abs(a1-(24+a2))
+	else
+		aomlaag = math.abs(a1+24-a2)
+	end
 	bomhoog = math.abs(b1-b2)
-	bomlaag = math.abs(b1-(60+b2))
+	if b1 > b2 then
+		if i == 4 then print("hello world", b1,b2) end
+		bomlaag = math.abs(b1-(60+b2))
+	else
+		if i == 4 then print(b2) end
+		bomlaag = math.abs(b1+60-b2)
+	end
 	if i == 4 then 
 	print(a1, b1, "en", a2, b2) 
 	print(aomhoog)
 	print(aomlaag)
 	print(bomhoog)
 	print(bomlaag)
-	end
+	end--]]
 	
 	
 	if aomhoog < aomlaag then
